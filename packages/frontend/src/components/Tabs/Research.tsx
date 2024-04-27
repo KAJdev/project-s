e9a38ba41e2d585cb4cc50991bfd50217fc5b7c3
kaj <40004347+KAJdev@users.noreply.github.com>
2024-04-27 02:14:42 -0800

    research bug fixes

--- packages/frontend/src/components/Tabs/Research.tsx
+++ packages/frontend/src/components/Tabs/Research.tsx
@@ -17,7 +17,7 @@ const TECHNOLOGIES = [
     id: "scanning",
     name: "Scanning",
     description:
-      "The ability to scan stars and carriers for resources and ships.",
+      "Increases the scanning radius of stars and carriers around stars by 1 LY per level.",
     formula: "scanning + 2 LY",
     affects: ["Stars", "Carriers"],
     icon: <Radar size={26} />,
@@ -25,7 +25,8 @@ const TECHNOLOGIES = [
   {
     id: "hyperspace",
     name: "Hyperspace Range",
-    description: "The ability to travel between stars.",
+    description:
+      "Increases the maximum carrier travel distance between stars by 1 LY per level.",
     formula: "hyperspace + 3 LY",
     affects: ["Carriers"],
     icon: <Rocket size={26} />,
@@ -33,7 +34,7 @@ const TECHNOLOGIES = [
   {
     id: "terraforming",
     name: "Terraforming",
-    description: "The ability to terraform stars for increased resources.",
+    description: "Increases the base resources of a star by 5 per level.",
     formula: "resources + (terraforming * 5)",
     affects: ["Stars"],
     icon: <Sprout size={26} />,
@@ -41,7 +42,7 @@ const TECHNOLOGIES = [
   {
     id: "experimentation",
     name: "Experimentation",
-    description: "Affects how big breakthroughs are.",
+    description: "Increases the points in breakthroughs by 72 per level.",
     formula: "experimentation * 72",
     affects: ["Research"],
     icon: <Microscope size={26} />,
@@ -57,7 +58,8 @@ const TECHNOLOGIES = [
   {
     id: "banking",
     name: "Banking",
-    description: "Additional cash earned each production cycle.",
+    description:
+      "Increases the amount of cash produced each production cycle by $75 per level.",
     formula: "banking * 75",
     affects: ["Cash"],
     icon: <DollarSign size={26} />,
@@ -66,7 +68,7 @@ const TECHNOLOGIES = [
     id: "manufacturing",
     name: "Manufacturing",
     description:
-      "Increases the amount of ships stars produced each production cycle.",
+      "Increases ships stars produce each production cycle by 5 per industry per level.",
     formula: "industry * (manufacturing + 5)",
     affects: ["Stars"],
     icon: <Building size={26} />,
@@ -77,23 +79,27 @@ function ResearchItem({
   technology,
   player,
   game,
+  playerScience,
 }: {
   technology: (typeof TECHNOLOGIES)[number];
   player: Player;
   game: Game;
+  playerScience: number;
 }) {
   const points = player.research_points?.[technology.id] ?? 0;
   const currentLevel = player.research_levels?.[technology.id] ?? 0;
-  const cost = game.settings[`${technology.id}_cost`];
+  const cost = game.settings[`${technology.id}_cost`] * currentLevel;
   const progress = points / cost;
   const [loading, setLoading] = useState(false);
 
   const isResearching = player.research_queue?.[0] === technology.id;
 
+  const eta = Math.ceil((cost - points) / playerScience);
+
   return (
     <div
       className={classes(
-        "bg-white/10 px-3 py-2 flex relative gap-4 border border-dotted items-center h-[5rem] z-0"
+        "bg-white/10 px-3 py-2 flex relative gap-4 border border-dotted items-center h-[5.7rem] z-0"
       )}
       style={{
         borderColor: isResearching ? player.color : "transparent",
@@ -107,25 +113,25 @@ function ResearchItem({
         }}
       />
       <div className="shrink-0 z-10">{technology.icon}</div>
-      <div className="flex flex-col gap-1 h-full items-start z-10">
-        <div className="text-lg flex gap-3 items-center">
-          <span className="bg-[#315796] text-sm p-0.5 px-2">
+      <div className="flex flex-col gap-0.5 h-full items-start z-10">
+        <div className="text-lg flex gap-2 items-center">
+          <span className="bg-[#315796] text-sm p-[1px] px-2">
             LVL {currentLevel}
           </span>
           {technology.name}
         </div>
-        <p className="text-xs text-[10px] opacity-75">
+        <p className="text-xs text-[9px] sm:text-[11px] opacity-75">
           {technology.description}
         </p>
       </div>
-      <div className="flex flex-col gap-1 items-center ml-auto w-[8rem] shrink-0 z-10">
-        <p className="text-xs opacity-75">
+      <div className="flex flex-col gap-2 items-center ml-auto w-[8rem] shrink-0 z-10">
+        <p className="text-sm opacity-75">
           {points} / {cost}
         </p>
         <Button
           className={classes(
             "justify-center w-full",
-            isResearching ? "p-0" : "p-2"
+            isResearching ? "p-0" : "p-2 text-xs"
           )}
           variant={isResearching ? "transparent" : "outline"}
           disabled={isResearching}
@@ -137,7 +143,9 @@ function ResearchItem({
             );
           }}
         >
-          {isResearching ? "Researching..." : "Set Active"}
+          {isResearching
+            ? `ETA: ${eta} hour${eta > 1 ? "s" : ""}`
+            : "Set Active"}
         </Button>
       </div>
     </div>
@@ -149,6 +157,12 @@ export function Research() {
   const scan = scanStore((state) => state.scan);
   const game = useGame(scan?.game);
   if (!self) return null;
+
+  const playerScience =
+    scan?.stars
+      .filter((s) => s.occupier === self.id)
+      .reduce((acc, s) => acc + (s.science ?? 0), 0) ?? 0;
+
   return (
     <div className="flex flex-col gap-2 p-4">
       {TECHNOLOGIES.map((technology) => (
@@ -157,6 +171,7 @@ export function Research() {
           technology={technology}
           player={self}
           game={game}
+          playerScience={playerScience}
         />
       ))}
     </div>
